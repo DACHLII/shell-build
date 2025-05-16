4ecdbcb37b659fe5d5b474e55462bdd6c1cd48af
DACHLII <diyakrishna139@gmail.com>
2025-05-16 17:16:33 -0500

    testing step 2

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -11,15 +12,29 @@ int main(int argc, char *argv[]) {
 
   // REPL loop
   // Wait for user input
+  bool running = true;
   char input[100];
-  while( input != "exit")
+  while(running)
   {
     printf("$ ");
     //read
     fgets(input, 100, stdin);
     // null terminate the input
     input[strlen(input) - 1] = '\0';
-    printf("%s: command not found\n", input);
+    // check for exit
+    if(input == "exit 0")
+    {
+      exit(0);
+    }
+    else if (input == "exit 1")
+    {
+      exit(1);
+    }
+    else
+    {
+        printf("%s: command not found\n", input);
+    }
+  
     setbuf(stdout, NULL);
 
   }
