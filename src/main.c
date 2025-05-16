d594d5d7d6480525c81e83548963265d4053dc39
DACHLII <diyakrishna139@gmail.com>
2025-05-16 16:45:09 -0500

    testing step 2

--- src/main.c
+++ src/main.c
@@ -7,13 +7,14 @@ int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
 
   // Uncomment this block to pass the first stage
- printf("$ ");
+
 
   // REPL loop
   // Wait for user input
   char input[100];
   while( input != "exit")
   {
+    printf("$ ");
     //read
     fgets(input, 100, stdin);
     // null terminate the input
