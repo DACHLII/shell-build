01fb47a190784307e48899443bb269eacf5e124d
DACHLII <diyakrishna139@gmail.com>
2025-05-16 18:45:03 -0500

    testing step 2

--- src/main.c
+++ src/main.c
@@ -63,7 +63,7 @@ int main(int argc, char *argv[]) {
           token = strtok(NULL, " ");
           
         } 
-        printf("%s",echo);
+        printf("%s\n",echo);
       }
       else
       {
