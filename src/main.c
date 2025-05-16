ca9ce8b7152beb4b61ed697ecaf7d0510157e899
DACHLII <diyakrishna139@gmail.com>
2025-05-16 16:42:18 -0500

    testing step 2

--- src/main.c
+++ src/main.c
@@ -7,12 +7,23 @@ int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
 
   // Uncomment this block to pass the first stage
-  printf("$ ");
+ // printf("$ ");
 
+  // REPL loop
   // Wait for user input
   char input[100];
-  fgets(input, 100, stdin);
-  input[strlen(input) - 1] = '\0';
-  printf("%s: command not found\n", input);
+  while( input != "exit")
+  {
+    //read
+    fgets(input, 100, stdin);
+    // null terminate the input
+    input[strlen(input) - 1] = '\0';
+    printf("%s: command not found\n", input);
+    setbuf(stdout, NULL);
+
+  }
+  
+
+
   return 0;
 }
